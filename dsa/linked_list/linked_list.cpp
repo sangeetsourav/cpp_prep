1aeef7676360bff4dd49373d99d873fa4865d93a
Sangeet Sunderroy <sangeetsourav@gmail.com>
2024-08-08 16:11:59 +0000

    Added linked_list insertion at beginning and printing functions

--- dsa/linked_list/linked_list.cpp
+++ dsa/linked_list/linked_list.cpp
@@ -24,21 +24,62 @@ For linked lists:
 struct Node{
     int data;
     Node* next;
+};
+
+Node* head;    // Global, Pointer to the head node
+
+void insert_at_beginning(int x)
+{
+    // Create the node
+    Node * temp = new Node;
+    temp->data = x;
+    temp->next = NULL;
+    
+    // If list is empty
+    if(head==NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        temp->next = head;
+        head = temp;
+    }
+    
+    // Smaller version
+    // temp->next = head
+    // head = temp;
+}
+
+void print_linked_list()
+{
+    Node * temp = head;
+    
+    while(temp != NULL)
+    {
+        std::cout<<temp->data<<' ';
+        temp = temp->next;
+    }
+    
+    std::cout<<"\n";
 }
 
 int main()
 {   
-    // Pointer to the head node
-    Node* A;
-    A = NULL;
+    // Empty list
+    head = NULL; 
     
-    // Allocate memory for a node
-    // temp is not the node, its a pointer to the actual Node location
-    Node* temp = new Node;
+    std::cout<<"How many numbers?\n";
+    int n{};
+    std::cin >> n;
     
-    temp->data = 2;
-    temp->next = NULL;
-    
-    A = temp; // Now A points to the newly created node.
+    int x{};
+    for(int i = 0; i < n; i++)
+    {
+        std::cout<<"Enter the number: \n";
+        std::cin >> x;
+        insert_at_beginning(x);
+        print_linked_list();
+    }
     
 }
