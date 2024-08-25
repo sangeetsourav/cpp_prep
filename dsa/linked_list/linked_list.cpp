8e4cf14a88b7e3729f798a499f15619b70957b65
Sangeet Sunderroy <sangeetsourav@gmail.com>
2024-08-25 08:29:28 +0000

    added insert_at and delete_at

--- dsa/linked_list/linked_list.cpp
+++ dsa/linked_list/linked_list.cpp
@@ -51,10 +51,99 @@ void insert_at_beginning(int x)
     // head = temp;
 }
 
+void insert_at(int x, int pos)
+{
+    // Create the node
+    Node* temp = new Node;
+    temp->data = x;
+    
+    // If list is empty
+    if(head==NULL)
+    {
+        head = temp;
+        temp->next = NULL;
+    }
+    else
+    {
+        if(pos==1)
+        {
+            Node* next_node = head;
+            head = temp;
+            temp->next = next_node;
+        }
+        else
+        {
+            int index=1;
+            
+            Node* location = head;
+        
+            while(index < pos - 1)
+            {
+                location = location->next;
+                ++index;
+                if(location->next == NULL)
+                {
+                    break;
+                }
+            }
+            
+            Node* next_node = location->next;
+            location->next = temp;
+            temp->next = next_node;
+        }
+    }
+}
+
+void delete_at(int pos)
+{
+    // If list is empty
+    if(head==NULL)
+    {
+        return;
+    }
+    else
+    {
+        if(pos==1)
+        {
+            Node* next_node = head;
+            head = head->next;
+            // Deallocate deleted node
+            delete next_node;
+        }
+        else
+        {
+            int index=1;
+            
+            Node* location = head;
+        
+            while(index < pos - 1)
+            {
+                location = location->next;
+                ++index;
+                if(location->next == NULL)
+                {
+                    break;
+                }
+            }
+            
+            if(location->next != NULL)
+            {
+                Node* next_node = location->next;
+                location->next = location->next->next;
+                // Deallocate deleted node
+                delete next_node;
+            }
+        }
+    }    
+    
+}
+
 void print_linked_list()
 {
     Node * temp = head;
     
+    std::cout<<"List: ";
+        
     while(temp != NULL)
     {
         std::cout<<temp->data<<' ';
@@ -69,17 +158,22 @@ int main()
     // Empty list
     head = NULL; 
     
-    std::cout<<"How many numbers?\n";
-    int n{};
-    std::cin >> n;
-    
-    int x{};
-    for(int i = 0; i < n; i++)
-    {
-        std::cout<<"Enter the number: \n";
-        std::cin >> x;
-        insert_at_beginning(x);
-        print_linked_list();
-    }
+    // Insert
+    insert_at(1,1); //1
+    print_linked_list();
+    insert_at(10,1); //10, 1    
+    print_linked_list();
+    insert_at(3,2); //10,3,1
+    print_linked_list();
+    insert_at(4,3); //10,3,4,1
+    print_linked_list();
+    delete_at(1); //3,4,1
+    print_linked_list();
+    delete_at(2); //3,1
+    print_linked_list();
+    delete_at(100); //3,1
+    print_linked_list();    
+    delete_at(2); //3
+    print_linked_list();    
     
 }
